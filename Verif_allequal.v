bc1858e34663866dff4ebea25b599778d4eb70c7
dmittrey <dmitryzubahin@gmail.com>
2025-12-25 03:21:34 +0300

    [Verif] add proove stubs

--- Verif_allequal.v
+++ Verif_allequal.v
@@ -42,3 +42,71 @@ Require Import VST.floyd.proofauto. (* Verifiable C and its Floyd proof-automati
 Require Import allequal. (* AST *)
 #[export] Instance CompSpecs : compspecs. make_compspecs prog. Defined.
 Definition Vprog : varspecs. mk_varspecs prog. Defined.
+
+(** *** Functional model *)
+
+Definition all_equal (contents : list Z) : Prop.
+Admitted.
+
+(** ** API spec *)
+
+Definition allequal_spec : ident * funspec :=
+DECLARE _allequal
+ WITH a: val, sh : share, contents : list Z, size: Z
+ PRE [ tptr tuint, tint ]
+   PROP (readable_share sh;
+         0 <= size <= Int.max_signed;
+         Forall (fun x => 0 <= x <= Int.max_unsigned) contents)
+   PARAMS (a; Vint (Int.repr size))
+   SEP (data_at sh (tarray tuint size) (map Vint (map Int.repr contents)) a)
+ POST [ tuint ]
+   EX r: Z,
+   PROP (r = 0 \/ r = 1;
+         (r = 0 -> all_equal contents);
+         (r = 1 -> exists j, 0 <= j < Zlength contents /\
+                            Znth j contents <> Znth 0 contents))
+   RETURN (Vint (Int.repr r))
+   SEP (data_at sh (tarray tuint size) (map Vint (map Int.repr contents)) a).
+
+(** *** main() spec *)
+
+Definition four_contents : list Z := [1; 2; 3; 4].
+
+Definition main_spec :=
+ DECLARE _main
+  WITH gv : globals
+  PRE  [] main_pre prog tt gv
+  POST [ tint ]
+     PROP()
+     RETURN (Vint (Int.repr 1))
+     SEP(TT).
+
+(** ** Packaging the specs *)
+
+Definition Gprog := [allequal_spec; main_spec].
+
+(** ** Proof of allequal *)
+
+Lemma body_allequal : semax_body Vprog Gprog f_allequal allequal_spec.
+Proof.
+  
+Admitted.
+
+(** ** Global variables and main() *)
+
+Lemma body_main : semax_body Vprog Gprog f_main main_spec.
+Proof.
+
+Admitted.
+
+(** ** Tying all the functions together *)
+
+#[export] Existing Instance NullExtension.Espec.
+
+Lemma prog_correct : semax_prog prog tt Vprog Gprog.
+Proof.
+  prove_semax_prog.
+
+  semax_func_cons body_allequal.
+  semax_func_cons body_main.
+Qed.
